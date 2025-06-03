2c27de59f96e70b21736a53ed5d1200d36d924d9
RashingPro <rashingpro@yandex.ru>
2025-06-03 18:58:54 +0300

    feat: add testing-only forseState prop for Header

--- src/components/ui/Header/index.tsx
+++ src/components/ui/Header/index.tsx
@@ -2,7 +2,15 @@
 
 import style from "./header.module.css";
 
-export default async function Header() {
+interface IHeader {
+    /**
+     * For testing only. Don't use in production
+     * @deprecated
+     */
+    forceState?: "logged" | "not-logged"
+}
+
+export default async function Header(props: IHeader) {
     return <header className={style.header}>
     </header>
 }
