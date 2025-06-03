dcea5ab8a7d618181afeab3539c60fc5bbaf69b1
RashingPro <rashingpro@yandex.ru>
2025-06-03 18:59:09 +0300

    feat(stories): add story for Header

---
+++ src/stories/Header.stories.tsx
@@ -0,0 +1,22 @@
+import Header from "@/components/ui/Header";
+import { Meta, StoryObj } from "@storybook/nextjs-vite";
+
+const meta: Meta<typeof Header> = {
+    title: "Header",
+    component: Header
+}
+export default meta;
+
+type Story = StoryObj<typeof meta>
+
+export const Standard: Story = {}
+export const LoggedIn: Story = {
+    args: {
+        forceState: "logged"
+    }
+}
+export const LoggedOut: Story = {
+    args: {
+        forceState: "not-logged"
+    }
+}
